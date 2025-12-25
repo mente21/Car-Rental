d98dfae84be0e6c2dbe12d023050e373cf7d8fa7
mente21 <mentesnotdebele09@gmail.com>
2025-12-26 01:16:35 +0300

    inital commite

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -133,7 +133,7 @@ const Footer = () => {
         className="flex flex-col md:flex-row gap-2 items-center
       justify-between py-5"
       >
-        <p>@ {new Date().getFullYear()} Brand.All rights reserved.</p>
+        <p>@ {new Date().getFullYear()} Mente's Car Rental. All rights reserved.</p>
         <ul className="flex items-center gap-4">
           <li>
             <a href="#">Privacy</a>
